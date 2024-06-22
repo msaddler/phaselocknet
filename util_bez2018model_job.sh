8984ac8bb848d4be888c33acce953ef5125ce4a8
Mark Saddler <msaddler@mit.edu>
2024-06-22 16:38:37 -0400

    generating stimuli/spkr_word_recognition/optimization/[train|valid]/tfrecords_IHC3000_sr20000 bez2018model nervegrams

--- util_bez2018model_job.sh
+++ util_bez2018model_job.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 #
 #SBATCH --job-name=bez2018model
-#SBATCH --out="slurm-%A_%a.out"
+#SBATCH --out="tslurm-%A_%a.out"
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=2000
 #SBATCH --nodes=1
 #SBATCH --time=2-00:00:00
 #SBATCH --exclude=node[017-094,097,098],dgx001,dgx002
-#SBATCH --array=0-42
+#SBATCH --array=0-999
 ##SBATCH --partition=mcdermott
-##SBATCH --partition=use-everything
+#SBATCH --partition=use-everything
 #SBATCH --requeue
 
-regex_src="stimuli/spkr_word_recognition/evaluation/speech_in_synthetic_textures/*.hdf5"
-dir_dst="stimuli/spkr_word_recognition/evaluation/speech_in_synthetic_textures/tfrecords_IHC3000"
-jobs_per_source_file=43
+regex_src="stimuli/spkr_word_recognition/optimization/train/*.hdf5"
+dir_dst="$VAST_SCRATCH_PATH/stimuli/spkr_word_recognition/optimization/train/tfrecords_IHC3000_sr20000"
+jobs_per_source_file=5
+# regex_src="stimuli/spkr_word_recognition/optimization/valid/*.hdf5"
+# dir_dst="$VAST_SCRATCH_PATH/stimuli/spkr_word_recognition/optimization/valid/tfrecords_IHC3000_sr20000"
+# jobs_per_source_file=5
 offset=0
 job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
 
