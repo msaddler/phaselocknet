e7ab9e5ef3495c9b4093a8d8af7b115d40064180
Mark Saddler <msaddler@mit.edu>
2024-06-23 09:09:17 -0400

    generating stimuli/sound_localization/optimization/train/tfrecords_IHC3000_sr20000 bez2018model nervegrams

--- util_bez2018model_job.sh
+++ util_bez2018model_job.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 #
 #SBATCH --job-name=bez2018model
-#SBATCH --out="tslurm-%A_%a.out"
+#SBATCH --out="yslurm-%A_%a.out"
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=2000
 #SBATCH --nodes=1
 #SBATCH --time=2-00:00:00
-#SBATCH --exclude=node[017-094,097,098],dgx001,dgx002
-#SBATCH --array=0-999
+##SBATCH --exclude=node[017-094,097,098],dgx001,dgx002
+#SBATCH --array=0-599
 ##SBATCH --partition=mcdermott
 #SBATCH --partition=use-everything
 #SBATCH --requeue
 
-regex_src="stimuli/spkr_word_recognition/optimization/train/*.hdf5"
-dir_dst="$VAST_SCRATCH_PATH/stimuli/spkr_word_recognition/optimization/train/tfrecords_IHC3000_sr20000"
-jobs_per_source_file=5
+# regex_src="stimuli/spkr_word_recognition/optimization/train/*.hdf5"
+# dir_dst="$VAST_SCRATCH_PATH/stimuli/spkr_word_recognition/optimization/train/tfrecords_IHC3000_sr20000"
+# jobs_per_source_file=5
 # regex_src="stimuli/spkr_word_recognition/optimization/valid/*.hdf5"
 # dir_dst="$VAST_SCRATCH_PATH/stimuli/spkr_word_recognition/optimization/valid/tfrecords_IHC3000_sr20000"
 # jobs_per_source_file=5
+# regex_src="stimuli/sound_localization/optimization/valid/*.hdf5"
+# dir_dst="$VAST_SCRATCH_PATH/stimuli/sound_localization/optimization/valid/tfrecords_IHC3000_sr20000"
+# jobs_per_source_file=3
+regex_src="stimuli/sound_localization/optimization/train/*.hdf5"
+dir_dst="$VAST_SCRATCH_PATH/stimuli/sound_localization/optimization/train/tfrecords_IHC3000_sr20000"
+jobs_per_source_file=3
 offset=0
 job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
 
