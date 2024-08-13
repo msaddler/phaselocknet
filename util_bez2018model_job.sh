c62afbefdfede4ccb36bbce50de7a6c6d276299b
Mark Saddler <msaddler@mit.edu>
2024-08-12 20:35:08 -0400

    generating optimization/[train|valid]/tfrecords_simplified for autocorr simplified cochlea models

--- util_bez2018model_job.sh
+++ util_bez2018model_job.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 #
 #SBATCH --job-name=bez2018model
-#SBATCH --out="trash/zslurm-%A_%a.out"
+#SBATCH --out="trash/slurm-%A_%a.out"
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=2000
 #SBATCH --nodes=1
 #SBATCH --time=2-00:00:00
 #SBATCH --exclude=node[017-094,097,098],dgx001,dgx002
-#SBATCH --array=0-99
+#SBATCH --array=0-199
 ##SBATCH --partition=mcdermott
-#SBATCH --partition=use-everything
+##SBATCH --partition=use-everything
 #SBATCH --requeue
 
-# regex_src="stimuli/spkr_word_recognition/evaluation/hopkins_moore_2009/*.hdf5"
-# dir_dst="$SCRATCH_PATH/stimuli/spkr_word_recognition/evaluation/hopkins_moore_2009/tfrecords_IHC3000_sr20000"
-# jobs_per_source_file=100
-regex_src="stimuli/sound_localization/evaluation/new_ears/*.hdf5"
-dir_dst="$SCRATCH_PATH/stimuli/sound_localization/evaluation/new_ears/tfrecords_IHC3000_sr20000"
-jobs_per_source_file=100
+regex_src="stimuli/sound_localization/optimization/train/*.hdf5"
+dir_dst="$VAST_SCRATCH_PATH/stimuli/sound_localization/optimization/train/tfrecords_simplified"
+jobs_per_source_file=1
 offset=0
 job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
 
